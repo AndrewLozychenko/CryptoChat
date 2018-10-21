fbdc697ede495c965a70c142780fbf3add382f9f
Andrew <andrii.lozychenko@nure.ua>
2018-10-21 09:41:58 +0300

    Change to express

--- core.js
+++ core.js
@@ -1,32 +1,14 @@
-var server = require('http').createServer();
-var io = require('socket.io')(server);
-var express = require('express');
+var express  = require('express');
 var app = express();
 
-server.listen(process.env.PORT || 8080);
-console.log('Server on');
+var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + "/public"));
 
 app.get("/",function(req,res){
-	res.render("test");
+    res.render("index");
 });
 
-var users;
-var id = 0;
-
-io.on('connection', (socket) =>
-{
-    console.log("Someone connected");
-    socket.emit('connected', {});
-    users[socket] = {'id':id,'socket':socket};
-    id++;
-
-    socket.on('message', function(data)
-    {
-        for(var i in users)
-        { 
-			i.socket.emit('message',{message:data.message});
-        }
-    });
-});								
+app.listen(port,function(){
+    console.log("Server on");
+});
